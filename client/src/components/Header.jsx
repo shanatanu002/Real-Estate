59f2214d9cebfd4e5ddac216281482d1dcf0aa57
shanatanu002 <bajpaishantanu2002@gmail.com>
2024-11-18 21:42:40 +0530

    modified2

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,8 +27,8 @@ export default function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Moons</span>
-            <span className='text-slate-700'>Truk</span>
+            <span className='text-slate-500'>Ash</span>
+            <span className='text-slate-700'>Estates</span>
           </h1>
         </Link>
         <form
